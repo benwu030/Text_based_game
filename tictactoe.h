a5b44b80c98b2d071cd749660a67c003bd7b0386
BenWu <benwu013@connect.hku.hk>
2021-03-23 01:07:34 +0800

    First minigame i.e. tic tac toe Minimax algorithm is implemented Set diffculty by changing the else statement in titactoe_main.cpp (AIMove -> AIMove_Random)

---
+++ tictactoe.h
@@ -0,0 +1,10 @@
+//tiktactoe.h
+#ifndef TIKTACTOE_H
+#define TIKTACTOE_H
+void TTT_Display(char board[3][4]);
+int TTT_CheckWin(char board[3][4]);
+void TTT_HandlePlayerInput(char board[3][4]);
+void TTT_AIMove(char board[3][4]);
+void TTT_AIMove_Random(char board[3][4]);
+
+#endif
