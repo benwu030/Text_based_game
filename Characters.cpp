2dfb729ca3996cdae752e39e6cf0242a623f2788
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2021-04-11 18:17:19 +0800

    New mini game

--- Characters.cpp
+++ Characters.cpp
@@ -10,14 +10,14 @@ struct Item{
 
 struct Character{
   string name;
-  int helath;
+  int health;
   int damage;
   int armour;
   int speed;
   string Current_location;
   Item item[15];
   Item equipped_item[4];
-}Chris{"Chris Tombow",100,30,20,10,"",{Gun_1},{}},Bosco{"Chris Tombow",100,5,20,10};
+}Chris{"Chris Tombow",100,30,20,10,"",{Gun_1},{}} , zero{Big Boss, 100, 5, 20, 10} , one{Mister Muscles, 100, 5, 20, 10} , two{Mister Hawkeye, 100, 5, 20, 10}, three{Mister Guard, 100, 5, 20, 10};
 
 
 
---
+++ guessobject.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void guessObject(int a){
+  string input, answer;
+  int count = 0;
+  if (a == 0){
+    answer = "Car";
+    cout << "        _______" << endl;
+    cout << "       //  ||\\ \\" << endl;
+    cout << " _____//___||_\\ \\___" << endl;
+    cout << " )  _          _    \\" << endl;
+    cout << " |_/ \\________/ \\___|" << endl;
+    cout << "___\\_/________\\_/______" << endl;
+  }
+  else if (a == 1){
+    answer = "Keyboard";
+    cout << " _____________________________________________________________________" << endl;
+    cout << "|                                                                     |" << endl;
+    cout << "| [_]   [_][_][_][_] [_][_][_][_] [_][_][_][_] [_][_][_] [_][_][_][_] |" << endl;
+    cout << "|                                         ___                         |" << endl;
+    cout << "| [ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][___] [_][_][_] [_][_][_][_] |" << endl;
+    cout << "| [__][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][  | [_][_][_] [_][_][_][ | |" << endl;
+    cout << "| [___][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][_|           [_][_][_][_| |" << endl;
+    cout << "| [_][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][______]    [_]    [_][_][_][ | |" << endl;
+    cout << "| [__][_][__][_____________________][__][_][_] [_][_][_] [____][_][_| |" << endl;
+    cout << "|_____________________________________________________________________|" << endl;
+    cout << "" << endl;
+  }
+  else if (a == 2){
+    answer = "Turtle";
+    cout << "               __" << endl;
+    cout << "    .,-;-;-,. /'_\\" << endl;
+    cout << "  _/_/_/_|_\\_\\) /" << endl;
+    cout << "'-<_><_><_><_>=/\\" << endl;
+    cout << "  `/_/====/_/-'\\_\\" << endl;
+    cout << "   \"\"     \"\"    \"\"" << endl;
+  }
+  else if (a == 3){
+    answer = "Pikachu";
+    cout << "`;-.          ___," << endl;
+    cout << "  `.`\\_...._/`.-\"`" << endl;
+    cout << "    \\        /      ," << endl;
+    cout << "    /()   () \\    .' `-." << endl;
+    cout << "   |)  .    ()\\  /   _.'" << endl;
+    cout << "   \\  -'-     ,; '. <" << endl;
+    cout << "    ;.__     ,;|   > \\" << endl;
+    cout << "   / ,    / ,  |.-'.-'" << endl;
+    cout << "  (_/    (_/ ,;|.<`" << endl;
+    cout << "    \\    ,     ;-`" << endl;
+    cout << "     >   \\    /" << endl;
+    cout << "    (_,-'`> .'" << endl;
+    cout << "         (_,'" << endl;
+  }
+  else if (a == 4){
+    answer = "Gun";
+    cout << " +--^----------,--------,-----,--------^-," << endl;
+    cout << " | |||||||||   `--------'     |          O" << endl;
+    cout << " `+---------------------------^----------|" << endl;
+    cout << "   `\\_,---------,---------,--------------'" << endl;
+    cout << "     / XXXXXX /'|       /'" << endl;
+    cout << "    / XXXXXX /  `\\    /'" << endl;
+    cout << "   / XXXXXX /`-------'" << endl;
+    cout << "  / XXXXXX /" << endl;
+    cout << " / XXXXXX /" << endl;
+    cout << "(________(   " << endl;
+  }
+  cout << endl;
+  cout << "Guess what is this object?" << endl;
+  cin >> input;
+  for (int i = 0; i < answer.length(); i++){
+    if (tolower(input[i]) == tolower(answer[i])){
+      count++;
+    }
+  }
+  if (count == answer.length()){
+    cout << "Correct!" << endl;
+  }
+  else{
+    cout << "Wrong!" << endl;
+  }
+}
+int main(){
+  guessObject(4);
+}
