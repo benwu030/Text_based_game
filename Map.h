c5b1e561b66c658a18653c4e6e3207f1f77131c8
BenWu <benwu013@connect.hku.hk>
2021-04-26 03:02:11 +0800

    Change jail to Jail

--- Map.h
+++ Map.h
@@ -1,7 +1,9 @@
 //Map.h
 #ifndef MAP_H
 #define MAP_H
+#include<iostream>
 #include<string>
+using namespace std;
 void Map_Respawn_Init(string Map[]);
 void Map_Lab_Init(string Map[]);
 void Display_Lab_Map(string Map[]);
@@ -9,7 +11,7 @@ void removePlayerLocation(string Map[]);
 void Current_At_Chemistry(string Map[]);
 void Current_At_Toilet(string Map[]);
 void Current_At_A1(string Map[]);
-void Current_At_jail(string Map[]);
+void Current_At_Jail(string Map[]);
 void Current_At_A6868(string Map[]);
 void Current_At_SecurityOffice(string Map[]);
 void Current_At_BioLab(string Map[]);
