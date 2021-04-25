b9aeb386d5882bc800f616e22f37d961f6f3cb54
BenWu <benwu013@connect.hku.hk>
2021-04-26 04:53:44 +0800

    Linked main_menu

--- Main.cpp
+++ Main.cpp
@@ -7,8 +7,8 @@
 #include "battle.cpp"
 #include "tictactoe.h"
 #include "guessObject.cpp"
-#include "readfile.cpp"
-
+#include "readfile.h"
+#include "Main_Menu.cpp"
 using namespace std;
 
 
@@ -17,8 +17,18 @@ using namespace std;
 int main(){
   string current, next;
   int option, difficulty = 1, win = 0;
+  print_MainMenu();
+  Main_character Chris;
+  string Map[31];
+  string choice;
+  bool flag = false;
+  while (!flag){
+    getline(cin,choice);
+    flag = MainMenu_Choice(choice,Map,Chris);
+  }
   cout << "You are Chris, a former police. Your daughter Alexandra has been abducted by unknown. You have searched her for 2 years and now all clues pointed to this corp - BioReincarnation(Bior.) Alexandra is said to be imprisoned in a lab inside an obsolete building. You are standing in front of the main entrance of the building. " << endl;
   cout << "Type anything and press Enter to continue: ";
+
   cin.get();
   Display_Lab_Map(map);
   cout << "This is the map of the building." << endl;
