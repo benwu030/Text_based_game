e72719a36cd10313b9f2c84f8f8cbb8323512833
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2021-04-27 02:23:28 +0800

    Mainmenuh

---
+++ Main_Menu.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+void SetColor(int value);
+void print_MainMenu();
+void print_Victory();
+void print_Lose();
+bool MainMenu_Choice(string choice,string Map[],Main_character &Chris, vector<Item> Item_Array, vector<Equiment> Equiment_Array);
+
+#endif
