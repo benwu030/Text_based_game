9a092861bbed2f8c165f8284732bd1e0fd865ea8
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2021-04-26 17:27:30 +0800

    WallBomb

--- ItemAndMonster.h
+++ ItemAndMonster.h
@@ -12,15 +12,15 @@ struct Equiment{
   int armour;
   int damage;
   int speed;
-}Gun_1{"Gun_1","GLock-18 Pistol", 0, 0, 10, -1} ,Gun_2{"Gun_2","AK47", 0, 0, 20, -3},Gun_3{"Gun_3","Shotgun", 0, 0, 15, -3}, Gun_4{"Gun_4","Sub Machine Gun", 0, 0, 10, -2},Armour_1{"Armour_1","Army Boots", 0, 5, 0, 10},Armour_2{"Armour_2","Police Bullet Proof Vest", 0, 10, 0, -10},Armour_3{"Armour_3","Army Gloves", 0, 5, 0, -2},Armour_4{"Armour_4","helmet", 0, 10, 0, -5};
+}Gun_1{"Gun_1","GLock-18 Pistol", 0, 0, 10, -1} ,Gun_2{"Gun_2","AK47", 0, 0, 20, -3},Gun_3{"Gun_3","Shotgun", 0, 0, 15, -3}, Gun_4{"Gun_4","Sub Machine Gun", 0, 0, 10, -2},Armour_1{"Armour_1","Army Boots", 0, 5, 0, 10},Armour_2{"Armour_2","Police Bullet Proof Vest", 0, 10, 0, -5},Armour_3{"Armour_3","Army Gloves", 0, 5, 0, -2},Armour_4{"Armour_4","helmet", 0, 10, 0, -3};
 vector<Equiment> Equiment_Array = {Gun_1,Gun_2,Gun_3,Gun_4,Armour_1,Armour_2,Armour_3,Armour_4};
 struct Item{
   string identifier;
   string name;
   int value;
   int type;// type 1 = healing potion type 2 = grenade
-}heal_1{"heal_1","Bandage",10,1},heal_2{"heal_2","Energy Drink",5,1},heal_3{"heal_3","Mysterious Potion",20,1},grenade_1{"grenade_1","High Explosive Grenade",30,2},heal_4{"heal_4","Mini Bandage", 10}, device{"device","Hacking Device",0,0},daughter{"daughter", "Alexandra",0,0};
-vector <Item> Item_Array ={heal_1,heal_2,heal_3,heal_4,grenade_1,daughter};
+}heal_1{"heal_1","Bandage",15,1},heal_2{"heal_2","Energy Drink",10,1},heal_3{"heal_3","Mysterious Potion",20,1},grenade_1{"grenade_1","M67 Grenade",20,2}, grenade_2{"grenade_2","Gas Bomb",15, 2} heal_4{"heal_4","Mini Bandage", 10}, device{"device","Hacking Device",0,0},daughter{"daughter", "Alexandra",0,0},bomb("bomb","WallBomb",0,0);
+vector <Item> Item_Array ={heal_1,heal_2,heal_3,heal_4,grenade_1,grenade_2,daughter,device,bomb};
 
 struct Main_character{
   string name;
@@ -43,5 +43,5 @@ struct Monster{
   int armour;
   int speed;
 
-}zero{"Big Boss", 100, 20, 20, 10} , one{"Mister Muscles", 100, 30, 20, 10} , two{"Mister Hawkeye", 70, 20, 15, 20}, three{"Mister Guard", 50, 10, 5, 50}, four{"Mister Alien", 50, 10, 3, 50};
+}zero{"Big Boss", 100, 40, 40, 10} , one{"Mister Muscles", 100, 30, 20, 10} , two{"Mister Hawkeye", 70, 20, 15, 20}, three{"Mister Guard", 50, 10, 5, 50}, four{"Mister Alien", 50, 10, 5, 50};
 #endif
